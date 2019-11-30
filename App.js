ba2da52a3e0dc603fbe43b032a6688bd3bc5f436
alex <camward@yandex.ru>
2019-11-30 18:18:03 +0400

    список элементов

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Navbar } from './src/Navbar'
 import { AddTodo } from './src/AddTodo'
+import { Todo } from './src/Todo'
 
 export default function App() {
   const [todos, setTodos] = useState([])
@@ -34,6 +35,12 @@ export default function App() {
       <Navbar title='Todo App!' />
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
+
+        <View>
+          {todos.map(todo => (
+            <Todo todo={todo} key={todo.id} />
+          ))}
+        </View>
       </View>
     </View>
   )
