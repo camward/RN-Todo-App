e72562a52925f62a3e003c9271cb6ae269bf8322
alex <camward@yandex.ru>
2019-11-30 17:23:19 +0400

    state

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Navbar } from './src/Navbar'
 import { AddTodo } from './src/AddTodo'
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = title => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos(todos.concat([ newTodo ]))
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
   return (
     <View>
       <Navbar title='Todo App!' />
       <View style={styles.container}>
-        <AddTodo />
+        <AddTodo onSubmit={addTodo} />
       </View>
     </View>
   )
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { View, StyleSheet, TextInput, Button } from 'react-native'
 
-export const AddTodo = props => {
+export const AddTodo = ({ onSubmit }) => {
+  const pressHandler = () => {
+    onSubmit('Test todo')
+  }
+
   return (
     <View style={styles.block}>
       <TextInput style={styles.input} />
-      <Button title='Добавить' />
+      <Button title='Добавить' onPress={pressHandler} />
     </View>
   )
 }
