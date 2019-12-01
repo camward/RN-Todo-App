429184f0495872c445023cca3bc82e80e9b273aa
alex <camward@yandex.ru>
2019-12-01 15:19:10 +0400

    компонент карточки

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import { THEME } from '../theme'
+import { AppCard } from '../components/ui/AppCard'
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View>
-      <Text>{todo.title}</Text>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title='Ред.' />
+      </AppCard>
+
       <View style={styles.buttons}>
         <View style={styles.button}>
           <Button title='Назад' onPress={goBack} color={THEME.GREY_COLOR} />
@@ -27,7 +32,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
+  card: {
+    marginBottom: 20,
+    padding: 15
+  },
   button: {
     width: '40%'
+  },
+  title: {
+    fontSize: 20
   }
 })
