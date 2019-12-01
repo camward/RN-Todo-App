b012ea01297f06cad7416ec03993fad17ca48444
alex <camward@yandex.ru>
2019-12-01 16:17:13 +0400

    изменение элементов

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import { View, StyleSheet, TextInput, Button, Modal } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, TextInput, Button, Modal, Alert } from 'react-native'
 import { THEME } from '../theme'
 
-export const EditModal = ({ visible, onCancel }) => {
+export const EditModal = ({ visible, onCancel, value, onSave }) => {
+  const [title, setTitle] = useState(value)
+
+  const saveHandler = () => {
+    if (title.trim().length < 3) {
+      Alert.alert(
+        'Ошибка!',
+        `Минимальная длинна названия 3 символа. Сейчас ${
+          title.trim().length
+        } символов.`
+      )
+    } else {
+      onSave(title)
+    }
+  }
+
   return (
     <Modal visible={visible} animationType='slide' transparent={false}>
       <View style={styles.wrap}>
         <TextInput
+          value={title}
+          onChangeText={setTitle}
           style={styles.input}
           placeholder='Введите название'
           autoCapitalize='none'
@@ -19,7 +36,7 @@ export const EditModal = ({ visible, onCancel }) => {
             onPress={onCancel}
             color={THEME.DANGER_COLOR}
           />
-          <Button title='Сохранить' />
+          <Button title='Сохранить' onPress={saveHandler} />
         </View>
       </View>
     </Modal>
